1b5ddadbcbafd493e742f12187812b593c44dad2
miluski <maksymilian.sowula@gmail.com>
2024-07-29 13:39:45 +0200

    feat: Implemented footer frontend

--- frontend/src/app/footer/footer.component.ts
+++ frontend/src/app/footer/footer.component.ts
@@ -1,11 +1,38 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterModule } from '@angular/router';
+import { ProductTypesService } from '../product-types.service';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css',
-  standalone: true
+  imports: [MatIconModule, RouterModule],
+  standalone: true,
 })
 export class FooterComponent {
-
+  private accountType: string = 'personal';
+  private cardType: string = 'standard';
+  private depositType: string = 'timely';
+  constructor(private productTypesService: ProductTypesService) {}
+  ngOnInit(): void {
+    this.productTypesService.currentAccountType.subscribe(
+      (accountType) => (this.accountType = accountType)
+    );
+    this.productTypesService.currentCardType.subscribe(
+      (cardType) => (this.cardType = cardType)
+    );
+    this.productTypesService.currentDepositType.subscribe(
+      (depositType) => (this.depositType = depositType)
+    );
+  }
+  changeAccountType(accountType: string): void {
+    this.productTypesService.changeAccountType(accountType);
+  }
+  changeCardType(cardType: string): void {
+    this.productTypesService.changeCardType(cardType);
+  }
+  changeDepositType(depositType: string): void {
+    this.productTypesService.changeDepositType(depositType);
+  }
 }
--- frontend/src/app/single-deposit/single-deposit.component.ts
+++ frontend/src/app/single-deposit/single-deposit.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductTypesService } from '../product-types.service';
 
 @Component({
   selector: 'app-single-deposit',
   templateUrl: './single-deposit.component.html',
-  styleUrl: './single-deposit.component.css'
+  styleUrl: './single-deposit.component.css',
 })
-export class SingleDepositComponent {
-
+export class SingleDepositComponent implements OnInit {
+  private depositType: string = 'timely';
+  constructor(private productTypesService: ProductTypesService) {}
+  ngOnInit(): void {
+    this.productTypesService.currentDepositType.subscribe(
+      (depositType) => (this.depositType = depositType)
+    );
+  }
+  changeDepositType(depositType: string): void {
+    this.productTypesService.changeDepositType(depositType);
+  }
 }
