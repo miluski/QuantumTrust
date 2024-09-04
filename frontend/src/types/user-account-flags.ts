8e25232f10bd63d66828b977519a3024bb38ac53
miluski <maksymilian.sowula@gmail.com>
2024-09-04 19:37:37 +0200

    fix: Putted all of user fields into classes types making user data manipulating shortener

---
+++ frontend/src/types/user-account-flags.ts
@@ -0,0 +1,16 @@
+export class UserAccountFlags {
+  isIdentifierValid!: boolean;
+  isEmailValid!: boolean;
+  isPhoneNumberValid!: boolean;
+  isNameValid!: boolean;
+  isSurnameValid!: boolean;
+  isPeselValid!: boolean;
+  isIdentityDocumentTypeValid!: boolean;
+  isIdentityDocumentSerieValid!: boolean;
+  isAddressValid!: boolean;
+  isPasswordValid!: boolean;
+  isRepeatedPasswordValid!: boolean;
+  isAccountTypeValid!: boolean;
+  isAccountCurrencyValid!: boolean;
+  isVerificationCodeValid!: boolean;
+}
---
+++ frontend/src/types/user-account.ts
@@ -0,0 +1,16 @@
+export class UserAccount {
+  identifier!: number;
+  type!: string;
+  currency!: string;
+  email!: string;
+  phoneNumber!: number;
+  name!: string;
+  surname!: string;
+  pesel!: number;
+  identityDocumentType!: string;
+  identityDocumentSerie!: string;
+  address!: string;
+  password!: string;
+  repeatedPassword!: string;
+  verificationCode!: number;
+}
