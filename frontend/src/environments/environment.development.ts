8cec43e6f70f9f39136042589f4a0b2b6d98ea4a
Maksymilian Sowula <50271575+miluski@users.noreply.github.com>
2024-11-10 23:22:56 +0100

    Delete frontend/src/environments directory
    Signed-off-by: Maksymilian Sowula <50271575+miluski@users.noreply.github.com>

--- frontend/src/environments/environment.development.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  apiUrl: 'http://192.168.0.11:8080/api',
-  production: false,
-};
--- frontend/src/environments/environment.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  apiUrl: 'https://quantum-trust.cc/api',
-  production: true,
-};
